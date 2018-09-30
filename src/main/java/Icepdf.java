a812138d8044af5312559024a59757312a018f95
liujingming <redtomatoL@163.com>
2018-09-30 13:50:18 +0800

    读取网络流中的pdf文件转成图片

--- src/main/java/Icepdf.java
+++ src/main/java/Icepdf.java
@@ -2,17 +2,27 @@ import java.awt.image.BufferedImage;
 import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import javax.imageio.ImageIO;
 
+import org.apache.commons.lang3.StringUtils;
 import org.icepdf.core.pobjects.Document;
 import org.icepdf.core.util.GraphicsRenderingHints;
 /*
  * pdf 转 图片
  */
 public class Icepdf {
-    public static void pdf2Pic(String pdfPath, String path){
+    public static void pdf2Pic(String pdfPath, String path, String url) throws MalformedURLException {
         Document document = new Document();
-        document.setFile(pdfPath);
+        if(StringUtils.isNotBlank(pdfPath)){
+            URL pdfUrl = new URL(url);
+            document.setUrl(pdfUrl);
+
+        }else{
+            document.setFile(pdfPath);
+        }
+
         float scale = 2.5f;//缩放比例
         float rotation = 0f;//旋转角度
 
@@ -32,8 +42,9 @@ public class Icepdf {
         }
         document.dispose();
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
         String filePath = "D:\\workspace\\testpdftoimage\\22D8867F4A04928CACC065125F0BBC2A_20180929160331.pdf";
-        pdf2Pic(filePath, "D:\\");
+        String url ="http://172.0.16.102:8080/WLZFZP/22D8867F4A04928CACC065125F0BBC2A_20180929160331.pdf";
+        pdf2Pic(filePath, "D:\\",url);
     }
 }
